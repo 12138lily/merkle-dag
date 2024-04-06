c2281bfae9cfcfc47cbe2292bf35b45ed886ec87
Lily <2272951630@qq.com>
2024-04-06 22:38:34 +0800

    second

--- dag.go
+++ dag.go
@@ -28,21 +28,19 @@ type Object struct {
 	Data  []byte
 }
 
-type Link struct {
-	Name string
-	Hash []byte
-	Size int
-}
-
-type Object struct {
-	Links []Link
-	Data  []byte
-}
-
-func Add(store KVStore, node Node, hp HashPool) []byte {
+func Add(store KVStore, node Node, h hash.Hash) []byte {
 	// TODO 将分片写入到KVStore中，并返回Merkle Root
-	return processNode(node, store, h)
-
+	obj := &Object{}
+	switch node.Type() {
+	case FILE:
+		obj = handleFile(node, store, h)
+		break
+	case DIR:
+		obj = handleDir(node, store, h)
+		break
+	}
+	jsonObj, _ := json.Marshal(obj)
+	return computeHash(jsonObj, h)
 }
 
 // 处理节点，返回默克尔树根
